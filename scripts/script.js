c25ffc0ed1a9ac2b2c541b8e874b137e67273523
Magda Danielyan <mdanielyan50@gmail.com>
2025-05-13 13:18:02 +0200

    background and logo

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const pizzas= [
+    { 
+    "name": "Margherita",
+    "price": 12.99 ,
+    "description": "Tomate, Mozzarella, Basilikum" ,
+},
+        
+    { 
+    "name" : "Salami ",
+    "price": 14.99 ,
+    "description" : "Tomatensoße, Mozzarella, Salami" ,
+},
+        
+
+    { 
+    "name" : "Vegetarisch ",
+    "price": 13.49 ,
+    "description" : "Paprika, Zucchini, Pilze, Tomaten" ,
+},
+    
+{ 
+      "name"  : "BBQ Chicken ",
+    "price": 13.49 ,
+    "description" : "BBQ-Soße, Hähnchen, Mais, Zwiebeln" ,
+},
+
+
+{ 
+    "name"  : " Meeresfrüchte",
+    "price": 13.49 ,
+    "description" : "Garnelen, Tintenfisch, Knoblauch, Petersilie" ,
+},
+]
